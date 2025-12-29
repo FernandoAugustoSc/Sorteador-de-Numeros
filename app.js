2e854345b5963fd1cb4b39a9c9d339c335214b93
FernandoAugustoSc <fernandosc2142000@gmail.com>
2025-12-29 01:31:40 -0300

    corrigido funcionalidade dos botoes 'Sortear' e 'Reiniciar'

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
-    
+
     if (!quantidade) {
     alert('Preencha o campo "Quantidade de números"!');
     return;
@@ -62,19 +62,9 @@ function alterarStatusBotaoReiniciarSortear(){
     if(botaoReiniciar.classList.contains('container__botao-desabilitado')){
         botaoReiniciar.classList.remove('container__botao-desabilitado');
         botaoReiniciar.classList.add('container__botao');
-    } else {
-        botaoReiniciar.classList.remove('container__botao');
-        botaoReiniciar.classList.add('container__botao-desabilitado');
+        document.getElementById('btn-reiniciar').removeAttribute('disabled');
     }
-    if(botaoSortear.classList.contains('container__botao')){
-        botaoSortear.classList.remove('container__botao');
-        botaoSortear.classList.add('container__botao-desabilitado');
-    } else {
-        botaoSortear.classList.remove('container__botao-desabilitado');
-        botaoSortear.classList.add('container__botao');
-    } 
 }
-
 
 function reiniciar(){
     let botaoReiniciar = document.getElementById('btn-reiniciar');
@@ -88,12 +78,10 @@ function reiniciar(){
         ate.value = '';
         botaoReiniciar.classList.remove('container__botao');
         botaoReiniciar.classList.add('container__botao-desabilitado');
+        document.getElementById('btn-reiniciar').setAttribute('disabled', true );
+
         botaoSortear.classList.remove('container__botao-desabilitado');
         botaoSortear.classList.add('container__botao');
         resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`;
-    } else {
-        botaoSortear.classList.remove('container__botao');
-        botaoSortear.classList.add('container__botao-desabilitado');
-        resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
-    } 
+    }
 }
